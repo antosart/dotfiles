cb4afe50e0e5bafc313fe7508ffaaeb4fe556d93
forscher21 <antonio217@web.de>
2017-09-24 09:44:14 +0200

    Add indium to emacs configuration

--- emacs/config-javascript.el
+++ emacs/config-javascript.el
@@ -2,9 +2,12 @@
 
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
-(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+;; (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
-(eval-after-load 'tern
-   '(progn
-      (require 'tern-auto-complete)
-      (tern-ac-setup)))
+;; (eval-after-load 'tern
+;;    '(progn
+;;       (require 'tern-auto-complete)
+;;       (tern-ac-setup)))
+
+(require 'indium)
+(add-hook 'js2-mode-hook #'indium-interaction-mode)
