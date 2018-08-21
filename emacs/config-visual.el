5faaf339c632a2f0dd2c30dd8dae0ca82e4454df
forscher21 <antonio217@web.de>
2018-08-21 11:13:08 +0200

    [emacs] fix projectile shortcuts

--- emacs/config-visual.el
+++ emacs/config-visual.el
@@ -57,7 +57,9 @@
   :ensure t
   :demand t
   :config
-  (projectile-global-mode))
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 (use-package helm-projectile
   :ensure t
