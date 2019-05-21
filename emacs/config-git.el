f35a302f231e7ea90eac24c9b9bc20e7d0e4beeb
forscher21 <antonio217@web.de>
2019-05-21 16:58:20 +0200

    [emacs] magit

--- emacs/config-git.el
+++ emacs/config-git.el
@@ -1,19 +1,75 @@
 (use-package magit
   :commands magit-get-top-dir
   :ensure t
+  :bind (("C-c g" . magit-status)
+         ("C-c C-g l" . magit-file-log)
+         ("C-c f" . magit-grep))
   :init
-  ;; Close popup when commiting - this stops the commit window
-  ;; hanging around
-  ;; From: http://git.io/rPBE0Q
-  (defadvice git-commit-commit (after delete-window activate)
-    (delete-window))
-  (defadvice git-commit-abort (after delete-window activate)
-    (delete-window))
+  (progn
+    ;; magit extensions
+    (use-package magit-blame
+      :bind ("C-c C-g b" . magit-blame-mode))
+
+    ;; we no longer need vc-git
+    (delete 'Git vc-handled-backends)
+
+    ;; Close popup when commiting - this stops the commit window
+    ;; hanging around
+    ;; From: http://git.io/rPBE0Q
+    (defadvice git-commit-commit (after delete-window activate)
+      (delete-window))
+
+    (defadvice git-commit-abort (after delete-window activate)
+      (delete-window))
+
+    ;; these two force a new line to be inserted into a commit window,
+    ;; which stops the invalid style showing up.
+    ;; From: http://git.io/rPBE0Q
+    (defun magit-commit-mode-init ()
+      (when (looking-at "\n")
+        (open-line 1)))
+
+    (add-hook 'git-commit-mode-hook 'magit-commit-mode-init))
   :config
-  (setq
+  (progn
+    ;; restore previously hidden windows
+    (defadvice magit-quit-window (around magit-restore-screen activate)
+      (let ((current-mode major-mode))
+        ad-do-it
+        ;; we only want to jump to register when the last seen buffer
+        ;; was a magit-status buffer.
+        (when (eq 'magit-status-mode current-mode)
+          (jump-to-register :magit-fullscreen))))
+
+    ;; (defun magit-maybe-commit (&optional show-options)
+    ;;   "Runs magit-commit unless prefix is passed"
+    ;;   (interactive "P")
+    ;;   (if show-options
+    ;;       (magit-key-mode-popup-committing)
+    ;;     (magit-commit)))
+
+    ;; (define-key magit-mode-map "c" 'magit-maybe-commit)
+
+    ;; magit settings
+    (setq
+     ;; use ido to look for branches
+     ;; magit-completing-read-function 'magit-ido-completing-read
+     ;; don't put "origin-" in front of new branch names by default
+     magit-default-tracking-name-function 'magit-default-tracking-name-branch-only
+     ;; open magit status in same window as current buffer
+     magit-status-buffer-switch-function 'switch-to-buffer
      ;; highlight word/letter changes in hunk diffs
-   magit-diff-refine-hunk t)
-  (setq git-commit-summary-max-length 72))
+     magit-diff-refine-hunk t
+     ;; ask me if I want to include a revision when rewriting
+     magit-rewrite-inclusive 'ask
+     ;; ask me to save buffers
+     magit-save-some-buffers t
+     ;; pop the process buffer if we're taking a while to complete
+     magit-process-popup-time 10
+     ;; ask me if I want a tracking upstream
+     magit-set-upstream-on-push 'askifnotset
+     git-commit-summary-max-length 72
+     )))
 
 (global-git-commit-mode)
 
